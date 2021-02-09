dd1499ef43c01abb624e9be8df04c87fd0e7d7ae
EricBalingit <EricBalingit@users.noreply.github.com>
2021-02-09 08:23:16 -0500

    add input event cycles to color picker

--- ui/js/ColorPicker.js
+++ ui/js/ColorPicker.js
@@ -1,5 +1,3 @@
-'use strict';
-
 require = require ( '../../require' );
 
 const   template = document.createElement ( 'template' ),
@@ -13,101 +11,240 @@ function hsvToHsl ( V, L ) {
         l = v - 0.5 * v * s,
         m = Math.min ( l, 1 - l );
     s = m ? ( v - l ) / m : 0;
-    Object.assign ( L, { h, s, l } );
+    L.h = h;
+    L.s = s;
+    L.l = l;
+}
+
+function rgbToHsv ( R, V ) {
+    let { r, g, b } = R,
+        v = Math.max ( r, g, b ),
+        c = v - Math.min ( r, g, b ),
+        h = c && (
+            v === r ? ( g - b ) / c : (
+                v === g ? 2 + ( b - r ) / c :
+                4 + ( r - g ) / c
+            )
+        );
+    V.h = ( h < 0 ? h + 6 : h ) / 6;
+    V.s = v ? c / v : 0;
+    V.v = v;
 }
 
 window.customElements.define ( 'color-picker', class ColorPicker extends HTMLElement {
 
-    connectedCallback () {}
+    #value = "";
+    #previousValue = "";
+    #offsets = null;
+    #hsv = null;
+    #hsl = null;
+    #rgb = null;
+    #hue = null;
+    #hPointer = null;
+    #satVal = null;
+    #satValComputed = null;
+    #changed = false;
+    #reverting = false;
+    #updating = false;
+    #setting = false;
+    #connected = false;
+    #shadow = null;
+    #inputValue = null;
+    #changeValue = null;
+
+    static get observedAttributes () {
+        return [ 'value' ];
+    }
+
+    get value () {
+        return this.#value;
+    }
+
+    set value ( val ) {
+        this.#inputValue ( val );
+    }
+
+    attributeChangedCallback ( name, old, value ) {
+        if ( !this.#setting ) {
+            this.value = value;
+        }
+    }
 
     constructor () {
         super ();
 
-        let hframe = NaN, svframe = NaN;
+        this.#inputValue = val => {
+            let update = false,
+                rgb = this.#rgb,
+                hsv = this.#hsv,
+                hsl = this.#hsl,
+                offsets = this.#offsets,
+                hPointer = this.#hPointer,
+                hue = this.#hue,
+                satVal = this.#satVal,
+                satValComputed = this.#satValComputed,
+                hColor = "",
+                svColor = "";
 
-        const   content = template.content.cloneNode ( true ),
-                h = content.querySelector ( '.h-pointer' ).style,
-                sv = content.querySelector ( '.sv-pointer' ).style,
-                svBackground = content.querySelector ( '.h' ).style,
-                hueInput = content.querySelector ( '.h-overlay' ),
-                svInput = content.querySelector ( '.sv-overlay' ),
-                bounds = { h: null, sv: null },
-                offsets = { h: { top: 0 }, sv: { top: 255, left: 0 } },
-                hsv = { h: 0, s: 0, v: 0 },
-                hsl = { h: 0, s: 0, l: 0 },
-                updateH = ts => {
-                    h.top = offsets.h.top + 'px';
-                    hsv.h = 0.00390625 * offsets.h.top;
-                    hsvToHsl ( hsv, hsl );
-                    sv.backgroundColor = `hsl(${hsl.h + 'turn'},${(hsl.s * 100) + '%'},${(hsl.l * 100) + '%'})`;
-                    svBackground.backgroundColor = `hsl(${hsl.h + 'turn'},100%,50%)`;
-                    let v = hsv.v;
-                    hsv.v = Math.sqrt ( 1.0 - hsv.v );
-                    hsvToHsl ( hsv, hsl );
-                    sv.borderColor = `hsl(0,0%,${(hsl.l * 100) + '%'})`;
-                    hsv.v = v;
-
-                    hframe = window.requestAnimationFrame ( updateH );
-                },
-                updateSV = ts => {
-                    sv.left = offsets.sv.left + 'px';
-                    sv.top = offsets.sv.top + 'px';
+            if ( this.#updating ) {
+                if ( offsets.h.top !== Number ( hPointer.top ) ) {
+                    update = true;
+                    hsl.h = hsv.h = 0.00390625 * offsets.h.top;
+                }
+                if ( offsets.sv.left !== Number ( satVal.left ) || offsets.sv.top !== Number ( satVal.top ) ) {
+                    update = true;
                     hsv.s = offsets.sv.left / 255;
                     hsv.v = ( 255 - offsets.sv.top ) / 255;
                     hsvToHsl ( hsv, hsl );
-                    sv.backgroundColor = `hsl(${hsl.h + 'turn'},${(hsl.s * 100) + '%'},${(hsl.l * 100) + '%'})`;
-                    let v = hsv.v;
-                    hsv.v = Math.sqrt ( 1.0 - hsv.v  );
-                    hsvToHsl ( hsv, hsl );
-                    sv.borderColor = `hsl(0,0%,${(hsl.l * 100) + '%'})`;
-                    hsv.v = v;
+                }
+                hColor = `hsl(${hsl.h}turn,100%,50%)`;
+                svColor = `hsl(${hsl.h}turn,${100*hsl.s}%,${100*hsl.l}%)`;
+            } else {
+                // check for computed color === this.#value
+                svColor = satVal.backgroundColor;
+                satVal.backgroundColor = val;
+                val = satValComputed.backgroundColor;
+                satVal.backgroundColor = svColor;
+                if ( update = val === this.#value ) {
+                    svColor = val;
+                    let [ r, g, b ] = val.split ( /[(,)]/ ).slice ( 1, 4 ).map ( n => Number ( n ) );
+                        rgb.r = r;
+                        rgb.g = g;
+                        rgb.b = b;
+                    rgbToHsv ( rgb, hsv );
+                    hColor = `hsl(${hsv.h}turn,100%,50%)`;
+                    offsets.h.top = 255 * ( 1 - hsv.h );
+                    offsets.sv.left = 255 * hsv.s;
+                    offsets.sv.top = 255 * ( 1 - hsv.v );
+                }
+            }
+            if ( update && ( this.#reverting || this.dispatchEvent (
+                new CustomEvent ( 'input', {
+                    detail: Object.freeze ( { inputValue: svColor, currentValue: this.#value } )
+                } )
+            ) ) ) {
+                hPointer.top = offsets.h.top + 'px';
+                satVal.left = offsets.sv.left + 'px';
+                satVal.top = offsets.sv.top + 'px';
+                hue.backgroundColor = hColor;
+                satVal.backgroundColor = svColor;
+                this.#setting = true;
+                this.#value = svColor;
+                this.setAttribute ( 'value', satValComputed.backgroundColor );
+                this.#setting = false;
+            }
+        };
 
-                    svframe = window.requestAnimationFrame ( updateSV );
-                },
-                pointermoveH = event => {
-                    offsets.h.top = Math.min ( Math.max ( 0, offsets.h.top + event.movementY ), 255 );
-                },
-                pointermoveSV = event => {
-                    offsets.sv.left = Math.min ( Math.max ( 0, offsets.sv.left + event.movementX ), 255 );
-                    offsets.sv.top = Math.min ( Math.max ( 0, offsets.sv.top + event.movementY ), 255 );
-                },
-                pointerupH = event => {
-                    window.cancelAnimationFrame ( hframe );
-                    window.removeEventListener ( 'pointermove', pointermoveH );
-                    window.removeEventListener ( 'pointerup', pointerupH );
-                },
-                pointerupSV = event => {
-                    window.cancelAnimationFrame ( svframe );
-                    window.removeEventListener ( 'pointermove', pointermoveSV );
-                    window.removeEventListener ( 'pointerup', pointerupSV );
-                },
-                pointerdownH = event => {
-                    bounds.h = event.target.getBoundingClientRect ();
-                    let pos = Math.min ( Math.max ( 0, event.clientY - bounds.h.top - 1 ), 255 );
-                    offsets.h.top = pos;
-                    hsv.h = 0.00390625 * pos;
-                    window.addEventListener ( 'pointermove', pointermoveH );
-                    window.addEventListener ( 'pointerup', pointerupH );
+        this.#changeValue = () => {
+            if ( this.#changed ) {
+                this.#changed = false;
+                if ( !this.dispatchEvent (
+                    new CustomEvent ( 'changed', {
+                        detail: Object.freeze ( { currentValue: this.#value, previousValue: this.#previousValue } )
+                    } )
+                ) ) { // if cancelled
+                    this.#reverting = true;
+                    this.value = this.#previousValue;
+                    this.#reverting = false;
+                }
+            }
+        };
 
-                    hframe = window.requestAnimationFrame ( updateH );
-                },
-                pointerdownSV = event => {
-                    bounds.sv = event.target.getBoundingClientRect ();
-                    let x = Math.min ( Math.max ( 0, event.clientX - bounds.sv.left - 1 ), 255 ),
-                        y = Math.min ( Math.max ( 0, event.clientY - bounds.sv.top - 1 ), 255 );
-                    offsets.sv.left = x;
-                    offsets.sv.top = y;
-                    hsv.s = x / 255;
-                    hsv.v = ( 255 - y ) / 255;
-                    window.addEventListener ( 'pointermove', pointermoveSV );
-                    window.addEventListener ( 'pointerup', pointerupSV );
+        this.#shadow = this.attachShadow ( { mode: 'closed' } );
+        this.#shadow.appendChild ( template.content.cloneNode ( true ) );
+    }
 
-                    svframe = window.requestAnimationFrame ( updateSV );
-                };
+    connectedCallback () {
+        if ( !this.#connected ) {
 
-                hueInput.addEventListener ( 'pointerdown', pointerdownH );
-                svInput.addEventListener ( 'pointerdown', pointerdownSV );
+            this.#connected = true;
 
-        this.attachShadow ( { mode: 'closed' } ).appendChild ( content );
+            let hframe = NaN, svframe = NaN;
+
+            const   shadow = this.#shadow,
+                    offsets = { h: { top: 0 }, sv: { top: 255, left: 0 } },
+                    hsv = { h: 0, s: 0, v: 0 },
+                    hsl = { h: 0, s: 0, l: 0.5 },
+                    rgb = { r: 0, g: 0, g: 0 },
+                    updateH = ts => {
+                        this.#updating = true;
+                        this.#inputValue ();
+                        this.#updating = false;
+                        hframe = window.requestAnimationFrame ( updateH );
+                    },
+                    updateSV = ts => {
+                        this.#updating = true;
+                        this.#inputValue ();
+                        this.#updating = false;
+                        svframe = window.requestAnimationFrame ( updateSV );
+                    },
+                    pointermoveH = event => {
+                        if ( !this.#changed ) {
+                            this.#changed = true;
+                            this.#previousValue = this.#value;
+                        }
+                        offsets.h.top = Math.min ( Math.max ( 0, offsets.h.top + event.movementY ), 255 );
+                    },
+                    pointermoveSV = event => {
+                        if ( !this.#changed ) {
+                            this.#changed = true;
+                            this.#previousValue = this.#value;
+                        }
+                        offsets.sv.left = Math.min ( Math.max ( 0, offsets.sv.left + event.movementX ), 255 );
+                        offsets.sv.top = Math.min ( Math.max ( 0, offsets.sv.top + event.movementY ), 255 );
+                    },
+                    pointerupH = event => {
+                        window.cancelAnimationFrame ( hframe );
+                        window.removeEventListener ( 'pointermove', pointermoveH );
+                        window.removeEventListener ( 'pointerup', pointerupH );
+                        this.#changeValue ();
+                    },
+                    pointerupSV = event => {
+                        window.cancelAnimationFrame ( svframe );
+                        window.removeEventListener ( 'pointermove', pointermoveSV );
+                        window.removeEventListener ( 'pointerup', pointerupSV );
+                        this.#changeValue ();
+                    },
+                    pointerdownH = event => {
+                        let bounds = event.target.getBoundingClientRect ();
+                        offsets.h.top = Math.min ( Math.max ( 0, Math.round ( event.clientY - bounds.top - 1 ) ), 255 );
+                        window.addEventListener ( 'pointermove', pointermoveH );
+                        window.addEventListener ( 'pointerup', pointerupH );
+                        hframe = window.requestAnimationFrame ( updateH );
+                    },
+                    pointerdownSV = event => {
+                        let bounds = event.target.getBoundingClientRect ();
+                        offsets.sv.left = Math.min ( Math.max ( 0, Math.round ( event.clientX - bounds.left - 1 ) ), 255 );
+                        offsets.sv.top = Math.min ( Math.max ( 0, Math.round ( event.clientY - bounds.top - 1 ) ), 255 );
+                        window.addEventListener ( 'pointermove', pointermoveSV );
+                        window.addEventListener ( 'pointerup', pointerupSV );
+                        svframe = window.requestAnimationFrame ( updateSV );
+                    },
+                    cancelH = event => {
+
+                    },
+                    cancelSV = event => {
+
+                    };
+
+            shadow.querySelector ( '.h-overlay' ).addEventListener ( 'pointerdown', pointerdownH );
+            shadow.querySelector ( '.sv-overlay' ).addEventListener ( 'pointerdown', pointerdownSV );
+
+            let svEl = shadow.querySelector ( '.sv-pointer' );
+
+            this.#hsv = hsv;
+            this.#hsl = hsl;
+            this.#rgb = rgb;
+            this.#offsets = offsets;
+            this.#hPointer = shadow.querySelector ( '.h-pointer' ).style;
+            this.#hue = shadow.querySelector ( '.h' ).style;
+            this.#satVal = svEl.style;
+            this.#satValComputed = getComputedStyle ( svEl );
+
+            this.#value = this.#satValComputed.backgroundColor;
+            this.#setting = true;
+            this.setAttribute ( 'value', this.#value );
+            this.#setting = false;
+        }
     }
 } );
